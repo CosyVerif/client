52fdf27af0eee81c2b3fc408da1446c85dcd601e
Alban Linard <alban@linard.fr>
2016-08-18 16:15:05 +0200

    Use environment variables to detect branch.

--- src/cosy/client/init_spec.lua
+++ src/cosy/client/init_spec.lua
@@ -56,8 +56,8 @@ local function make_false_token (subject, contents, duration)
   })
 end
 
-local branch
-do
+local branch = assert (os.getenv "COSY_BRANCH" or os.getenv "WERCKER_GIT_BRANCH")
+if not branch or branch == "master" then
   local file = assert (io.popen ("git rev-parse --abbrev-ref HEAD", "r"))
   branch = assert (file:read "*line")
   file:close ()
